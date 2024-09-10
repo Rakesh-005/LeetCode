6a461173985ed3f90499bc2ddb542f294112b01d
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2024-09-10 22:21:57 +0530

    [55] [Time Beats: 62.26%] [Memory Beats: 35.80%] - LeetPush

---
+++ 55-Jump-Game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        p=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if i+nums[i]>=p:
+                p=i
+        return p==0
