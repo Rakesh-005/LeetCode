7cad5714f3a3d2a13edf51cb5bce0b06de3c6ea9
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-01-04 17:52:52 +0530

    [1930] [Time Beats: 98.46%] [Memory Beats: 32.97%] - LeetPush

--- 1930-Unique-Length-3-Palindromic-Subsequences.py
+++ 1930-Unique-Length-3-Palindromic-Subsequences.py
@@ -3,6 +3,5 @@ class Solution:
         c=0
         for e in set(s):
             i,j=s.find(e),s.rfind(e)
-            if j>i+1:
-                c+=len(set(s[i+1:j]))
+            c+=len(set(s[i+1:j]))
         return c
