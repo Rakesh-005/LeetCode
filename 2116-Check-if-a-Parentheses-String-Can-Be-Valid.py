8839f613f0591d9b8ff4be5a71bbda53656e3203
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-01-12 11:49:17 +0530

    [2116] [Time Beats: 74.64%] [Memory Beats: 30.84%] - LeetPush

---
+++ 2116-Check-if-a-Parentheses-String-Can-Be-Valid.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s)%2==1:
+            return False
+        op=cp=0
+        for i in range(len(s)):
+            op+=1 if s[i]=='(' or locked[i]=='0' else -1
+            if op<0:
+                return False
+        for i in range(len(s)-1,-1,-1):
+            cp+=1 if locked[i]=='0' or s[i]==')' else -1
+            if cp<0:
+                return False
+        return True
