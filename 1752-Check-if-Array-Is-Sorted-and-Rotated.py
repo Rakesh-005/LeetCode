a58e2af2ea3ad56dc3f53cecc8409ca2869cbe9e
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-02-02 15:55:13 +0530

    [1752] [Time Beats: 100.00%] [Memory Beats: 42.72%] - LeetPush

--- 1752-Check-if-Array-Is-Sorted-and-Rotated.py
+++ 1752-Check-if-Array-Is-Sorted-and-Rotated.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def check(self, nums):
-        if sorted(nums) == nums:
+        a=sorted(nums)
+        if a == nums:
             return True
             
         for i in range(len(nums)):
             nums.append(nums.pop(0))
-            if nums == sorted(nums):
+            if nums == a:
                 return True
         return False
         
