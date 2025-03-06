aeec3c3d94fd8b21983a35fcb43543ff606fd098
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-03-06 11:37:45 +0530

    [2965] [Time Beats: 72.80%] [Memory Beats: 45.00%] - LeetPush

--- Find-Missing-and-Repeated-Values.py
+++ Find-Missing-and-Repeated-Values.py
@@ -1,14 +1,13 @@
+from typing import List
+from collections import Counter
+
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        l=[]
-        l1=[float('-inf')]*2
-        for i in grid:
-            for j in i:
-                l.append(j)
-        for i in set(l):
-            if l.count(i)==2:
-                l1[0]=i
-        for i in range(1,len(l)+1):
-            if i not in l:
-                l1[1]=i
-        return l1
+        l = [num for row in grid for num in row]  # Flatten grid
+        count = Counter(l)  # Get counts of each number
+        n = len(l)
+        
+        repeated = next(num for num, freq in count.items() if freq == 2)
+        missing = next(i for i in range(1, n + 1) if i not in count)
+
+        return [repeated, missing]
