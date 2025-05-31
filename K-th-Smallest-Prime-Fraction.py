6b5f3fcf2b0a06ffea80c18c1168b281f7b9a0d4
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-05-31 23:36:58 +0530

    [786] [Time Beats: 62.47%] [Memory Beats: 81.71%] - LeetPush

--- K-th-Smallest-Prime-Fraction.py
+++ K-th-Smallest-Prime-Fraction.py
@@ -1,9 +1,11 @@
 class Solution:
     def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
         mh=[]
-        for i in range(len(arr)):
-            for j in range(i+1,len(arr)):
-                heapq.heappush(mh,(arr[i]/arr[j],(arr[i],arr[j])))
-        for i in range(k):
-            a,b=heapq.heappop(mh)[1]
-        return [a,b]
+        for i in range(len(arr)-1):
+            heapq.heappush(mh,(arr[i]/arr[-1],(i,len(arr)-1)))
+        for i in range(k-1):
+            i,j=heapq.heappop(mh)[1]
+            if j-1>i:
+                heapq.heappush(mh,(arr[i]/arr[j-1],(i,j-1)))
+        a,b=heapq.heappop(mh)[1] 
+        return [arr[a],arr[b]]
