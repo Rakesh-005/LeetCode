5ab19372c4c0ec7defad92711fcedf10d9c6f606
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-06-15 17:28:30 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 25.96%] - LeetPush

--- Search-a-2D-Matrix.py
+++ Search-a-2D-Matrix.py
@@ -1,6 +1,6 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         for row in matrix:
-            if row[-1] >= target:
+            if row[-1] >= target or target<=row[0]:
                 return target in row
         return False
