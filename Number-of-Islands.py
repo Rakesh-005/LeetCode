6d30f0ea8babe8929a0d284819f69bbd67c353df
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-08-07 17:27:21 +0530

    [200] [Time Beats: 90.33%] [Memory Beats: 81.92%] - LeetPush

---
+++ Number-of-Islands.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numIslands(self, grid):
+        if not grid or not grid[0]:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] != '1':
+                return
+            grid[r][c] = '0'  # Mark as visited (use '0' instead of '#')
+            dfs(r + 1, c)  # Down
+            dfs(r - 1, c)  # Up
+            dfs(r, c + 1)  # Right
+            dfs(r, c - 1)  # Left
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1':
+                    dfs(r, c)
+                    count += 1
+
+        return count
