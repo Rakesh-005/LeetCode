5e944ffbf18693bb0ab5d72684c3c03cd2badc82
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-11-24 11:07:58 +0530

    [322] [Time Beats: 62.29%] [Memory Beats: 57.02%] - LeetPush

--- Coin-Change.py
+++ Coin-Change.py
@@ -1,14 +1,9 @@
 class Solution:
-    def coinChange(self,coins, amount):
-        # Initialize DP array with 'inf', except for dp[0] = 0
-        dp = [float('inf')] * (amount + 1)
-        dp[0] = 0
-
-        # Build the dp array
-        for a in range(1, amount + 1):
-            for coin in coins:
-                if a - coin >= 0:
-                    dp[a] = min(dp[a], dp[a - coin] + 1)
-
-        # If amount can't be formed, return -1
-        return dp[amount] if dp[amount] != float('inf') else -1
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        min_coins = [amount + 1] * (amount + 1)
+        min_coins[0] = 0
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    min_coins[i] = min(min_coins[i], 1 + min_coins[i - c])
+        return min_coins[-1] if min_coins[-1] != amount + 1 else -1
