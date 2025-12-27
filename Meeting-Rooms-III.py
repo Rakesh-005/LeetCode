c3640b3addb249680cea924c0b9e137f6b8db60f
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2025-12-27 23:40:39 +0530

    [2402] [Time Beats: 9.42%] [Memory Beats: 97.00%] - LeetPush

--- Meeting-Rooms-III.py
+++ Meeting-Rooms-III.py
@@ -1,18 +1,18 @@
-class Solution:
-    def mostBooked(self, n: int, m: List[List[int]]) -> int:
-        r, c = [0]*n, [0]*n # rooms, counter
-        for s, e in sorted(m):
-            found = 0
-            for i,f in enumerate(r):
-                if f <= s:
-                    r[i] = e
-                    c[i] += 1
-                    found = 1
-                    break
-
-            if not found:
-                q = r.index(min(r))
-                r[q] += e-s
-                c[q] += 1
-
-        return c.index(max(c))
+1class Solution:
+2    def mostBooked(self, n: int, m: List[List[int]]) -> int:
+3        r, c = [0]*n, [0]*n # rooms, counter
+4        for s, e in sorted(m):
+5            found = 0
+6            for i,f in enumerate(r):
+7                if f <= s:
+8                    r[i] = e
+9                    c[i] += 1
+10                    found = 1
+11                    break
+12
+13            if not found:
+14                q = r.index(min(r))
+15                r[q] += e-s
+16                c[q] += 1
+17
+18        return c.index(max(c))
