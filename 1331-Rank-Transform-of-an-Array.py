3b73443ef1768770228009d98f757871c0129088
Rakesh Sarma Ponukupati <150577167+Rakesh-005@users.noreply.github.com>
2026-07-12 20:57:08 +0530

    [1331] [Time Beats: 56.79%] [Memory Beats: 12.35%] - LeetPush

--- 1331-Rank-Transform-of-an-Array.py
+++ 1331-Rank-Transform-of-an-Array.py
@@ -8,6 +8,4 @@ class Solution(object):
         arr1=sorted(list(set(arr)))
         for i in range(len(arr1)):
             d[arr1[i]]=i+1
-        for i in range(len(arr)):
-            arr[i]=d[arr[i]]
-        return arr
+        return [d[arr[i]] for i in range(len(arr))]
